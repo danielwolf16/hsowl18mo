b60e7b94a45ca01fb2429a9390737dbc84ceb8d5
danielwolfm <16danielwolf@gmail.com>
2018-10-09 08:22:36 +0200

    First day development. Just view and pseudo functionality. App is craching when adding new members.

---
+++ app/src/main/java/com/mo/hsowl18/hsowl18mo/ActivitiesListActivity.java
@@ -0,0 +1,13 @@
+package com.mo.hsowl18.hsowl18mo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivitiesListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activities_list);
+    }
+}
--- app/src/main/java/com/mo/hsowl18/hsowl18mo/MainActivity.java
+++ app/src/main/java/com/mo/hsowl18/hsowl18mo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.mo.hsowl18.hsowl18mo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void showMembersActivity(View view) {
+        Intent intent = new Intent(this, MemberListActivity.class);
+        startActivity(intent);
+    }
+
+    public void showActivitiesActivity(View view) {
+        Intent intent = new Intent(this, ActivitiesListActivity.class);
+        startActivity(intent);
+    }
+
 }
